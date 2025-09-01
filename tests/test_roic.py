ecdaa484b737c34750c45fa1d7ef389713cfbdf3
chenxu10 <shenxu2011@gmail.com>
2025-08-31 21:40:11 -0400

    working

--- tests/test_roic.py
+++ tests/test_roic.py
@@ -1,5 +1,11 @@
+import pytest
 from src.siegfried import roic_calculator as rc
 
 def test_calculate_roic():
     ticker = "UNH"
-    assert rc.calculate_roic(ticker) == {}
+    assert isinstance(rc.calculate_roic(ticker),dict)
+
+
+def test_yf_exception():
+    with pytest.raises(Exception):
+        rc.calculate_roic()
