f458d3908cca4fccce75edad4954545ed9242092
chenxu10 <shenxu2011@gmail.com>
2025-08-31 22:06:07 -0400

    [B]Fix the yfinance balance sheet and income by update yfinance version

--- tests/test_roic.py
+++ tests/test_roic.py
@@ -4,9 +4,3 @@ from src.siegfried import roic_calculator as rc
 def test_calculate_roic():
     ticker = "UNH"
     assert isinstance(rc.calculate_roic(ticker),dict)
-
-
-def test_yf_exception():
-    ticker = "UNH"
-    with pytest.raises(Exception):
-        rc.calculate_roic(ticker)
