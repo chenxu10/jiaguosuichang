387438f35eb253f6cde44f9bc2ca359b21b0313b
chenxu10 <shenxu2011@gmail.com>
2025-08-31 21:42:42 -0400

    working

--- src/siegfried/roic_calculator.py
+++ src/siegfried/roic_calculator.py
@@ -1,6 +1,6 @@
 def calculate_roic(ticker: str) -> dict:
     try:
-        a = 2
+        return {}
     except Exception as e:
         roic = 9999
         return {'tikcer':ticker,'roic':roic,'e':e}
