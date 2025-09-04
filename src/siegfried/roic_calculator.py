78e46c9053026d32d9bb51f506f82d08ca11fee1
chenxu10 <shenxu2011@gmail.com>
2025-09-04 18:34:31 -0400

    working

--- src/siegfried/roic_calculator.py
+++ src/siegfried/roic_calculator.py
@@ -4,9 +4,10 @@ def calculate_roic(ticker: str) -> dict:
     try:
         stock = yf.Ticker(ticker)
         balance_sheet = stock.balance_sheet
-        print(balance_sheet)
-        if balance_sheet.empty:
-            return {"ticker": ticker, "roic": None, "error": "No financial data available"}
+        balance_sheet.to_csv("unh_balansheet.csv")
+        #print(balance_sheet)
+        #if balance_sheet.empty:
+        #    return {"ticker": ticker, "roic": None, "error": "No financial data available"}
     except Exception as e:
         roic = 9999
         return {'tikcer':ticker,'roic':roic,'e':e}
